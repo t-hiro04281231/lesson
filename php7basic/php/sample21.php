847ef18f7332e6c8d8bf8afb920316831d67640a
t-hiro04281231 <tobe-h@arrows-systems.co.jp>
2020-06-16 17:19:29 +0900

    初回コミット

---
+++ php7basic/php/sample21.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+<?php
+$zip = '987-6543';
+
+$zip = mb_convert_kana($zip,'a','UTF-8');
+if(preg_match("/\A\d{3}[-]\d{4}\z/",$zip)){
+    print('郵便番号:〒'. $zip);
+}else{
+    print('郵便番号を正しく入力してください');
+}
+?>
+</pre>
+</main>
+</body>    
+</html>
---
+++ php7basic/php/sample24/index.php
@@ -0,0 +1,30 @@
+<?php
+$value ='変数';
+setcookie('save_message','保存した値です',time()+60*60*24);
+?>
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+<a href="page02.php">Page02へ</a>
+</pre>
+</main>
+</body>    
+</html>
