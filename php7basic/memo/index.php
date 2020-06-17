676ef42fd063f5e931717745014b7b01cc42189d
t-hiro04281231 <tobe-h@arrows-systems.co.jp>
2020-06-17 17:08:29 +0900

    6/1717時更新

--- php7basic/memo/index.php
+++ php7basic/memo/index.php
@@ -20,14 +20,17 @@
 
 <!-- ここにプログラムを記述します -->
 <?php
-try{
-    $db = new PDO('mysql:dbname=mydb;localhost=127.0.0.1;charset=utf8',
-    'root','root');
-}catch(PDOException $e){
-    echo'DB接続エラー:'. $e->getMessage();
+require('dbconnect.php');
+if(isset($_REQUEST['page'])&& is_numeric($_REQUEST['page'])){
+$page = $_REQUEST['page'];
+}else{
+   $page = 1;
 }
-$memos =$db->query('SELECT*FROM memos ORDER BY id DESC');
+$start = 5*($page-1);
 
+$memos =$db->prepare('SELECT*FROM memos ORDER BY id DESC LIMIT ?,5');
+$memos->bindParam(1,$start,PDO::PARAM_INT);
+$memos->execute();
 ?>
 <article>
     <?php while($memo = $memos->fetch()):?>
@@ -35,6 +38,19 @@ $memos =$db->query('SELECT*FROM memos ORDER BY id DESC');
         <time><?php print($memo['created_at']);?></time>
         <hr>
     <?php endwhile;?>
+    <?php if($page >= 2):?>
+    <a href="index.php?page=<?php print($page-1); ?>"><?php print($page-1); ?>ページ目へ</a>
+    <?php endif; ?>
+    |
+    <?php
+    $counts =$db->query('SELECT COUNT(*) as cnt FROM memos');
+    $count =$counts->fetch();
+    $max_page = ceil($count['cnt']/5);
+    if($page < $max_page):
+    ?>
+
+   <a href="index.php?page=<?php print($page+1); ?>"><?php print($page+1); ?>ページ目へ</a>
+    <?php endif; ?>
 </article>
 
 </main>
